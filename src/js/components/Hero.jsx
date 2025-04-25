d251f0c5dbbea6a0d24e8ee6f4be26691b525af9
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-25 22:34:49 +0000

    change background on hero.

--- src/js/components/Hero.jsx
+++ src/js/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return ( 
-        <div className="container-fluid py-5">
+        <div className="container-fluid py-5 bg-light">
             <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
             <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nobis, harum saepe reprehenderit est nesciunt ipsa? Iure odit inventore dicta.</p>
             <button className="btn btn-primary btn-lg" type="button">Example Button</button>
