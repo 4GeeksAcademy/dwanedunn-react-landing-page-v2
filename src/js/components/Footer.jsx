52b86397a67ac9404e04cba0e0ae293b3b207d0e
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-26 13:44:26 +0000

    style refine footer to match design

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,12 +2,11 @@ const Footer = () => {
     return (
 
         <>
-        {/* <div className="container-fluid"> */}
             <footer className="navbar navbar-dark bg-dark text-light">
-                <p>Copyright &copy; Your website 2025</p>
+                <div className="d-flex p-2 w-100 justify-content-center align-items-center">
+                    <p>Copyright &copy; Your website 2025</p>
+                </div>
             </footer>
-            
-        {/* </div> */}
         </>
     );
 }
