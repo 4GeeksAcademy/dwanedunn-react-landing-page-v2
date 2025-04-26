87ebcfab7bda249a910a31dd0e95fcd518db2d5e
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-26 15:19:29 +0000

    fix card issue not loading

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,18 +1,18 @@
-const Card = (props) => {
-   return (
-      <>
-         <div className="card">
-            <img src={props.imageURL} className="card-img-top" alt={props.imageAlt}  />
-            <div className="card-body">
-               <h5 className="card-title">{props.cardTitle}</h5>
-               <p className="card-text">{props.content}</p>
-               <a href="#" className="btn btn-primary">
-                  Find out more!
-               </a>
-            </div>
-         </div>
-      </>
-   );
-}
+const Card = ({ imageURL, imageAlt, cardTitle, content }) => {
+  return (
+    <>
+      <div className="card">
+        <img src={imageURL} className="card-img-top" alt={imageAlt} />
+        <div className="card-body">
+          <h5 className="card-title">{cardTitle}</h5>
+          <p className="card-text">{content}</p>
+          <a href="#" className="btn btn-primary">
+            Find out more!
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Card;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 //include images into your bundle
 
-
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Hero from "./Hero";
@@ -10,16 +9,54 @@ import Card from "./Card";
 
 //create your first component
 const Home = () => {
-	return (
-		<>
-			<NavBar/>
-			<div className="container">
-			<Hero/>
-			</div>
-			<Card/>
-			<Footer/>
-		</>
-	);
+  const cards = [
+    {
+      imageURL: "https://via.placeholder.com/150",
+      imageAlt: "place holder",
+      title: "First Title",
+      content: "First Content",
+    },
+    {
+      imageURL: "https://via.placeholder.com/150",
+      imageAlt: "place holder",
+      title: "second Title",
+      content: "Second Content",
+    },
+    {
+      imageURL: "https://via.placeholder.com/150",
+      imageAlt: "place holder",
+      title: "third Title",
+      content: "third Content",
+    },
+    {
+      imageURL: "https://via.placeholder.com/150",
+      imageAlt: "place holder",
+      title: "Fourth Title",
+      content: "fourth Content",
+    },
+  ];
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <Hero />
+      </div>
+      <div className="d-flex">
+        {cards.map((card, index) => (
+          <div key={index}>
+            <Card
+              imageURL={card.imageURL}
+              imageAlt={card.imageAlt}
+              cardTitle={card.title}
+              content={card.content}
+            />
+          </div>
+        ))}
+      </div>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
